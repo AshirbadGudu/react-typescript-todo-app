3d93c436a0ee5543ba4d76f359e7bf47758a47d1
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-02 21:34:53 +0530

    AppHeader component created

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,12 @@
+import logo from "../logo.svg";
+
+export default function AppHeader() {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome To React TypeScript Todo App</p>
+      </header>
+    </>
+  );
+}
