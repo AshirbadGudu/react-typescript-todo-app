b0b21db8b64e80e69e3acd721bd80631f3ffb364
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-02 21:44:43 +0530

    All the created components used in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,51 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { FormEvent, useState } from "react";
+import "./App.css";
+import { AddTaskForm, AppHeader, TaskList } from "./components";
+
+type Task = {
+  id: number;
+  text: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [taskTxt, setTaskTxt] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (taskTxt.trim() === "") return;
+    setTasks((prev) => [
+      ...prev,
+      {
+        id: Date.now(),
+        text: taskTxt.trim(),
+      },
+    ]);
+    setTaskTxt("");
+  };
+
+  const deleteTask = (id: number) =>
+    setTasks((prev) => prev.filter((task) => task.id !== id));
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <AppHeader />
+      <main>
+        <AddTaskForm
+          onAddTask={addTask}
+          onChangeTaskText={(e) => setTaskTxt(e.target.value)}
+          taskTxt={taskTxt}
+        />
+        <section>
+          <ul>
+            {tasks.map((task) => (
+              <TaskList deleteTask={deleteTask} task={task} key={task.id} />
+            ))}
+          </ul>
+        </section>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
