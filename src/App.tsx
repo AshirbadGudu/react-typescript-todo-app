991598cba5202155c15f6663848f3e0e7682cb91
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-18 16:09:08 +0530

    In App.tsk useTodoStore added

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
 import { FormEvent, useState } from "react";
 import "./App.css";
 import { AddTaskForm, AppHeader, TaskList } from "./components";
-
-type Task = {
-  id: number;
-  text: string;
-};
+import { useTodoStore } from "./hooks";
 
 function App() {
-  const [taskTxt, setTaskTxt] = useState("");
-  const [tasks, setTasks] = useState<Task[]>([]);
-
-  const addTask = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (taskTxt.trim() === "") return;
-    setTasks((prev) => [
-      ...prev,
-      {
-        id: Date.now(),
-        text: taskTxt.trim(),
-      },
-    ]);
-    setTaskTxt("");
-  };
-
-  const deleteTask = (id: number) =>
-    setTasks((prev) => prev.filter((task) => task.id !== id));
+  const { tasks } = useTodoStore((state) => state);
 
   return (
     <div className="App">
       <AppHeader />
       <main>
-        <AddTaskForm
-          onAddTask={addTask}
-          onChangeTaskText={(e) => setTaskTxt(e.target.value)}
-          taskTxt={taskTxt}
-        />
+        <AddTaskForm />
         <section>
           <ul>
             {tasks.map((task) => (
-              <TaskList deleteTask={deleteTask} task={task} key={task.id} />
+              <TaskList task={task} key={task.id} />
             ))}
           </ul>
         </section>
